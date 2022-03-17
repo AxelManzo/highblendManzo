9ce61d0da5722cd162ef24a653eb1f2433dc4a1d
Axel <76486611+AxelManzo@users.noreply.github.com>
2022-03-16 22:51:19 -0300

    agregando cositas

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "NavBar.css";
+import logo from "../imgs/logo.jpg";
+
+const NavBar = () => {
+    return (
+        <div>
+        <img> src={logo} alt=""</img>
+        <h1>componente del header</h1>
+        </div>
+        
+    )
+}
+
+export default NavBar;
